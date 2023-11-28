1c0f92f012acbf2e5c49a455c39cc0a66c32f0c2
Devon Hua <huadevon@gmail.com>
2023-11-28 12:51:43 -0600

    .

--- config/connection.js
+++ config/connection.js
@@ -7,9 +7,9 @@ if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
   sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
+    process.env.qo0t9vacnffdkn39,
+    process.env.yvs8mt6wj5nz2qv9,
+    process.env.ypm0fx66k5jmsx0s,
     {
       host: "wcwimj6zu5aaddlj.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
       dialect: "mysql",
